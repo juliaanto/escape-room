76024f05b649be0ed3021e69c07cf9656a18c64d
Julia Antonova <1306577@mail.ru>
2021-12-01 11:21:16 +0300

    Создание репозитория

---
+++ src/components/app/app.jsx
@@ -0,0 +1,34 @@
+import * as S from './app.styled';
+
+import {
+  Route,
+  BrowserRouter as Router,
+  Switch,
+} from 'components/common/common';
+
+import Contacts from 'components/contacts/contacts';
+import DetailedQuest from 'components/detailed-quest/detailed-quest';
+import Home from 'components/home/home';
+import { ThemeProvider } from 'styled-components';
+import { appTheme } from './common';
+
+const App = () => (
+  <ThemeProvider theme={appTheme}>
+    <S.GlobalStyle />
+    <Router>
+      <Switch>
+        <Route exact path="/quest">
+          <DetailedQuest />
+        </Route>
+        <Route exact path="/contacts">
+          <Contacts />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </ThemeProvider>
+);
+
+export default App;
